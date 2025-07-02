66dc45ebb9877a0053e0272ff5c9347486a18684
Rodrigo Oliveira <rodrigo.silva.olviera369@gmail.com>
2025-07-02 16:01:44 -0300

    Initialize Spring Boot project for Voting Agenda application. - Add Gradle build script with dependencies for MongoDB, Kafka, and validation. - Configure Docker Compose for MongoDB service. - Include application properties and a sample test case. - Add .gitignore and .gitattributes files. - Integrate Gradle wrapper files.

---
+++ src/main/java/com/rodrigo/votingagenda/VotingAgendaApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigo.votingagenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingAgendaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VotingAgendaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rodrigo/votingagenda/VotingAgendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigo.votingagenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingAgendaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
